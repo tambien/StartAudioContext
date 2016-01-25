9d90fa2c85df510a1ec460a9ea3a8fe14f013726
Yotam Mann <yotammann@gmail.com>
2016-01-25 12:59:45 -0500

    initial commit

---
+++ StartAudioContext.js
@@ -0,0 +1,178 @@
+/**
+ *  StartAudioContext.js
+ *  @author Yotam Mann
+ *  @license http://opensource.org/licenses/MIT MIT License
+ *  @copyright 2016 Yotam Mann
+ */
+(function (root, factory) {
+	if (typeof define === "function" && define.amd) {
+		define([], factory);
+	 } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+	} else {
+		root.StartAudioContext = factory();
+  }
+}(this, function () {
+
+	/**
+	 * The StartAudioContext object
+	 */
+	var StartAudioContext = {
+		/**
+		 * The audio context passed in by the user
+		 * @type {AudioContext}
+		 */
+		context : null,
+		/**
+		 * The TapListeners bound to the elements
+		 * @type {Array}
+		 * @private
+		 */
+		_tapListeners : [],
+		/**
+		 * The callbacks bound to a valid tap event
+		 * @type {Array}
+		 * @private
+		 */
+		_onTapCallbacks : [],
+	};
+
+
+	/**
+	 * Set the context
+	 * @param {AudioContext} ctx
+	 * @returns {StartAudioContext}
+	 */
+	StartAudioContext.setContext = function(ctx){
+		StartAudioContext.context = ctx;
+		return StartAudioContext;
+	};
+
+	/**
+	 * Add a tap listener to the audio context
+	 * @param  {Array|Element|String|jQuery} element
+	 * @returns {StartAudioContext}
+	 */
+	StartAudioContext.on = function(element){
+		if (Array.isArray(element) || (NodeList && element instanceof NodeList)){
+			for (var i = 0; i < element.length; i++){
+				StartAudioContext.on(element[i]);
+			}
+		} else if (typeof element === "string"){
+			StartAudioContext.on(document.querySelectorAll(element));
+		} else if (element.jquery && typeof element.toArray === "function"){
+			StartAudioContext.on(element.toArray());
+		} else if (Element && element instanceof Element){
+			//if it's an element, create a TapListener
+			var tap = new TapListener(element, onTap);
+			StartAudioContext._tapListeners.push(tap);
+		} 
+		return StartAudioContext;
+	};
+
+	/**
+	 * Add a callback when there is a valid, context starting
+	 * tap event and the AudioContext is not already started.
+	 * @param  {Function} callback
+	 * @returns {StartAudioContext}
+	 */
+	StartAudioContext.onTap = function(callback){
+		StartAudioContext._onTapCallbacks.push(callback);
+		return StartAudioContext;
+	};
+
+	/**
+	 * returns true if the context is started
+	 * @return {Boolean}
+	 */
+	StartAudioContext.isStarted = function(){
+		return (StartAudioContext.context !== null && StartAudioContext.context.state === "running");
+	};
+
+	/**
+	 * @class  Listens for non-dragging tap ends on the given element
+	 * @param {Element} element
+	 * @internal
+	 */
+	var TapListener = function(element){
+
+		this._dragged = false;
+
+		this._element = element;
+
+		this._bindedMove = this._moved.bind(this);
+		this._bindedEnd = this._ended.bind(this);
+
+		element.addEventListener("touchmove", this._bindedMove);
+		element.addEventListener("touchend", this._bindedEnd);
+		element.addEventListener("mouseup", this._bindedEnd);
+	};
+
+	/**
+	 * drag move event
+	 */
+	TapListener.prototype._moved = function(e){
+		this._dragged = true;
+	};
+
+	/**
+	 * tap ended listener
+	 */
+	TapListener.prototype._ended = function(e){
+		if (!this._dragged){
+			onTap();
+		}
+		this._dragged = false;
+	};
+
+	/**
+	 * remove all the bound events
+	 */
+	TapListener.prototype.dispose = function(){
+		this._element.removeEventListener("touchmove", this._bindedMove);
+		this._element.removeEventListener("touchend", this._bindedEnd);
+		this._element.removeEventListener("mouseup", this._bindedEnd);
+		this._bindedMove = null;
+		this._bindedEnd = null;
+		this._element = null;
+	};
+
+	/**
+	 * Invoked the first time of the elements is tapped.
+	 * Creates a silent oscillator when a non-dragging touchend 
+	 * event has been triggered.
+	 */
+	function onTap(){
+
+		//start the audio context with a silent oscillator
+		if (StartAudioContext.context && !StartAudioContext.isStarted()){
+			var osc = StartAudioContext.context.createOscillator();
+			var silent = StartAudioContext.context.createGain();
+			silent.gain.value = 0;
+			osc.connect(silent);
+			silent.connect(StartAudioContext.context.destination);
+			var now = StartAudioContext.context.currentTime;
+			osc.start(now);
+			osc.stop(now+0.5);
+		}
+
+		//dispose all the tap listeners
+		if (StartAudioContext._tapListeners){
+			for (var i = 0; i < StartAudioContext._tapListeners.length; i++){
+				StartAudioContext._tapListeners[i].dispose();
+			}
+			StartAudioContext._tapListeners = null;
+		}
+
+		//invoke and dispose all the onTap handlers
+		if (StartAudioContext._onTapCallbacks){
+			for (var j = 0; j < StartAudioContext._onTapCallbacks.length; j++){
+				StartAudioContext._onTapCallbacks[j]();
+			}
+			StartAudioContext._onTapCallbacks = null;
+		}
+	}
+
+
+	return StartAudioContext;
+}));
